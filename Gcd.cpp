b5927046af14b0015563a12003afa70dd5cc37f1
Anish Aggarwal <anish.no-reply@not-an-email.com>
2019-12-26 15:21:01 +0530

    added

--- Gcd.cpp
+++ Gcd.cpp
@@ -1,24 +1,24 @@
-//A program to find GCD of two numbers 
-#include<bits/stdc++.h>
+//A program to find GCD
+#include <bits/stdc++.h>
 using namespace std;
-int GCD(int aa,int bb)
+int GCD(int aa, int bb)
 {
-	if(aa==0)
+	if (aa == 0)
 		return bb;
-	if(bb==0)
+	if (bb == 0)
 		return aa;
-	if(aa==bb)
+	if (aa == bb)
 		return aa;
-	if(aa>bb)
-		return GCD(aa-bb,bb);
-	return GCD(aa,bb-aa);
+	if (aa > bb)
+		return GCD(aa - bb, bb);
+	return GCD(aa, bb - aa);
 }
 int main()
 {
-	int a,b;
+	int a, b;
 	cout << "Enter two no.'s:\n";
 	cin >> a >> b;
-	int gcd=GCD(a,b);
+	int gcd = GCD(a, b);
 	cout << "GCD of two no.'s is:\n";
 	cout << gcd;
 }
