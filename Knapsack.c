1c2fee8dc79bf11a64e2cfe4368bc7669356b720
Sheetal Lalwani <68683093+Sheetal0601@users.noreply.github.com>
2020-10-02 14:40:35 +0530

    Update Knapsack.c

--- Knapsack.c
+++ Knapsack.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+
 int max(int a, int b) {
    if(a>b){
       return a;
@@ -7,19 +8,13 @@ int max(int a, int b) {
    }
 }
 int knapsack(int W, int wt[], int val[], int n) {
-   int i, w;
-   int knap[n+1][W+1];
-   for (i = 0; i <= n; i++) {
-      for (w = 0; w <= W; w++) {
-         if (i==0 || w==0)
-            knap[i][w] = 0;
-         else if (wt[i-1] <= w)
-            knap[i][w] = max(val[i-1] + knap[i-1][w-wt[i-1]], knap[i-1][w]);
-         else
-            knap[i][w] = knap[i-1][w];
-      }
-   }
-   return knap[n][W];
+  //using recursion 
+   if(n==0||W==0)
+      return 0;
+   if(wt[n-1]<=W)
+      return max(val[n-1]+knapsack(W-wt[n-1],wt.val,n-1) , knapsack(W,wt,val,n-1));
+   else if(wt[n-1]>W) 
+      return knapsack(W,wt,val.=,n-1);
 }
 int main() {
    int val[] = {20, 25, 40};
